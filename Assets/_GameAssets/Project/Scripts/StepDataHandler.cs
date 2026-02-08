f4dbdf0c8d41d9966763786bc4ea3606e58f27f9
Ogun Gundogdu <oguneg@gmail.com>
2026-02-08 02:41:17 +0100

    Basic display

---
+++ Assets/_GameAssets/Project/Scripts/StepDataHandler.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class StepDataHandler : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI stepTextTotal,
+        stepTextSession,
+        stepText30Mins,
+        stepTextToday,
+        stepTextYesterday,
+        stepTextLast7Days;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
