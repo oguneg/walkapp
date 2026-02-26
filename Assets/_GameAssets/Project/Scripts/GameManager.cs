1933a36d4630b08a35de6137e7640b7fadd2155e
Ogun Gundogdu <oguneg@gmail.com>
2026-02-26 14:55:24 +0100

    Scroll list

--- Assets/_GameAssets/Project/Scripts/GameManager.cs
+++ Assets/_GameAssets/Project/Scripts/GameManager.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     private bool isPermissionsGiven = false;
     public bool IsPermissionsGiven => isPermissionsGiven;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
 }
